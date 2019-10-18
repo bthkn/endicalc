5e062b60224f38d992f5109b0d30f60d263af7ae
bthkn <madeira578@icloud.com>
2019-10-18 15:30:25 +0300

    upd

--- src/js/tmp-envelope.js
+++ src/js/tmp-envelope.js
@@ -1,25 +1,7 @@
-
-// var paperCost = 0 // Стоимость бумаги
-// var printCost = 8 // стоимость печати 100%
-// var edition = 100 // Тираж
-// var kRent = 7.5 // Коэффициент рентабельности по визиткам соответствующего тиража
-// var lamCost = 0 // Стоимость ламинации соответствующего тиража
-// var cornerCost = 0 // стоимость скругления углов
-// var biegenCost = 0 // стоимость биговки соответствующего тиража (при выборе пользователем)
-// var flyerConst = 1 // А3: 1, А4: 2, А5: 4, А6: 8, 210*98: 6, 150*70: 12, 100*70: 18
-// var linesNumber = 52 // Количество полос
-// var coverCost = 6.85 // Стоимость бумаги для обложки
-// var skoba = 2.2 // стоимость скобы
-// var springCost = 0 // стоимость пружины
-// var grommetCost = 0 // стоимость люверса
-// var blockStd = 0 // календарный блок стандарт
-// var block85 = 0 // календарный блок 85*115
-// var begunok = 0 // стоимость бегунка
-
 var app = new Vue({
-  el: '#vizitCardsCalc',
+  el: '#envelopeCalc',
   data: {
-    currentProduct: 'Визитки',
+    currentProduct: 'Конверты',
     isLaminated: undefined,
     isCorners: undefined,
     edition: undefined,
@@ -39,11 +21,7 @@ var app = new Vue({
         "Splendorgel 300 гр",
         "TintoRetto 300 гр"
       ],
-      "paperFormat": {
-        "0":"A4",
-        "1":"A5",
-        "2":"A6"
-      },
+      "envelopeFormats": ["Е65", "С5", "С4"],
       "paperCosts": {
         "Офсетная 80 гр": 1.84,
         "Maxigloss 130гр": 2.21,
@@ -81,83 +59,37 @@ var app = new Vue({
           "100": 16
         }
       },
-      "additional": {
-        "corners" : {
-          "50": 175,
-          "100": 252,
-          "200": 315,
-          "300": 378,
-          "400": 441,
-          "500": 504,
-          "1000": 819,
-          "2000": 913.5
-        },
-        "laminat" : {
-          "50": 2205,
-          "100": 4400,
-          "200": 8800,
-          "300": 13200,
-          "400": 17600,
-          "500": 22000,
-          "1000": 0,
-          "2000": 0
-        },
-        "biegen" : {
-          "50": 141.75,
-          "100": 220.5,
-          "200": 378,
-          "300": 535.5,
-          "400": 693,
-          "500": 850.5,
-          "1000": 1638,
-          "2000": 0
-        }
-      },
       "kRentOf": {
-        "visitCards": {
-          "dizcrd": {
-            "4+0": {
-              "50": 3,
-              "100": 2.9,
-              "200": 2.8,
-              "300": 2.5,
-              "400": 2.2,
-              "500": 2,
-              "1000": 1.8,
-              "2000": 1.75
-            },
-            "4+4": {
-              "50": 3.5,
-              "100": 3.2,
-              "200": 3.2,
-              "300": 3,
-              "400": 2.9,
-              "500": 2.7,
-              "1000": 2.2,
-              "2000": 2
-            }
+        "envelope": {
+          "Е65": {
+            "10": 15,
+            "30": 0.3,
+            "50": 8.5,
+            "100": 6.6,
+            "200": 6,
+            "300": 5.5,
+            "500": 5,
+            "1000": 4
           },
-          "normal": {
-            "4+0": {
-              "50": 6,
-              "100": 4.45,
-              "200": 3.34,
-              "300": 2.9,
-              "400": 2.9,
-              "500": 2.8,
-              "1000": 2.6,
-              "2000": 2.1
-            },
-            "4+4": {
-              "50": 7,
-              "100": 4.9,
-              "200": 3.9,
-              "300": 3.6,
-              "400": 3.6,
-              "500": 3.5,
-              "1000": 2.9,
-              "2000": 2.1
-            }
+          "С5": {
+            "10": 15,
+            "30": 0.3,
+            "50": 8.5,
+            "100": 6.6,
+            "200": 6,
+            "300": 5.5,
+            "500": 5,
+            "1000": 4
+          },
+          "С4": {
+            "10": 11,
+            "30": 8,
+            "50": 6.5,
+            "100": 5,
+            "200": 4.5,
+            "300": 4.3,
+            "500": 4,
+            "1000": 3.4
           }
         }
       }
@@ -166,22 +98,17 @@ var app = new Vue({
   },
   methods: {
       getTotal() {
-        if (this.currentProduct == 'Визитки') {
-
-          var cardboardType = document.getElementById('cardboardType').value
-          var paper = document.getElementById('visitCards-paper').value
-          var color = document.getElementById('visitCards-color').value
-
-          var paperCost = this.db['paperCosts'][paper]
-          var printCost = this.db['printCosts'][color]['100']
-          var kRent = this.db['kRentOf']['visitCards'][cardboardType][""+color][this.edition]
-          var lamCost = this.isLaminated ? this.db['additional']['laminat'][this.edition] : 0
-          var cornerCost = this.isCorners ? this.db['additional']['corners'][this.edition] : 0
-          console.log(paperCost, printCost, kRent, lamCost, cornerCost)
-
-          var total = ((paperCost + printCost) / 24) * this.edition * kRent + lamCost + cornerCost
+        if (this.currentProduct == 'Конверты') {
           
-          alert('товар: Визитки\nкол-во: '+this.edition+'\nстоимость: '+total+' ('+total/this.edition+'\u20BD руб/шт)')
+          var format = document.getElementById('envelope-format').value
+
+          var envelopeCost = this.db['envelopeCost'][format]
+          var printCost = 1 // стоимость печати при 20% или при 100% 4+0 формата А3
+          var kRent = this.db['kRentOf']['envelope'][format]
+
+          var total = (envelopeCost + printCost) * this.edition * kRent
+          
+          alert('товар: Конверты\nкол-во: '+this.edition+'\nстоимость: '+total+' ('+total/this.edition+'\u20BD руб/шт)')
         }
       }
   }
--- src/js/tmp-notes.js
+++ src/js/tmp-notes.js
@@ -1,27 +1,10 @@
-
-// var paperCost = 0 // Стоимость бумаги
-// var printCost = 8 // стоимость печати 100%
-// var edition = 100 // Тираж
-// var kRent = 7.5 // Коэффициент рентабельности по визиткам соответствующего тиража
-// var lamCost = 0 // Стоимость ламинации соответствующего тиража
-// var cornerCost = 0 // стоимость скругления углов
-// var biegenCost = 0 // стоимость биговки соответствующего тиража (при выборе пользователем)
-// var flyerConst = 1 // А3: 1, А4: 2, А5: 4, А6: 8, 210*98: 6, 150*70: 12, 100*70: 18
-// var linesNumber = 52 // Количество полос
-// var coverCost = 6.85 // Стоимость бумаги для обложки
-// var skoba = 2.2 // стоимость скобы
-// var springCost = 0 // стоимость пружины
-// var grommetCost = 0 // стоимость люверса
-// var blockStd = 0 // календарный блок стандарт
-// var block85 = 0 // календарный блок 85*115
-// var begunok = 0 // стоимость бегунка
-
 var app = new Vue({
   el: '#notesCalc',
   data: {
     currentProduct: 'Блокноты',
     isLaminated: undefined,
     isCorners: undefined,
+    isBiegen: undefined,
     edition: undefined,
     db: {
       "paperTypes": [ 
